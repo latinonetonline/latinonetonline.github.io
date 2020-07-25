ccfa8915f93f804ed7cea67deacfe06625d6efdf
lautaro <lautaroecarro@gmail.com>
2020-07-25 00:00:27 -0300

    add telegram link

---
+++ src/components/sections/Groups.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Groups = () => {
+    return (
+        <div id="subscribe" className="section-padding">
+            <div className="container">
+                <div className="row justify-content-md-center">
+                    <div className="col-md-10 col-lg-7">
+                        <div className="subscribe-inner wow fadeInDown" data-wow-delay="0.3s">
+                            <h2 className="subscribe-title">Unite a nuestros grupos de WhatsApp y Telegram</h2>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6 ">
+                                    <h5><span role="img" aria-label="emojis">👇👇</span></h5>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#whatsapp">
+                                        <img src="/assets/img/wpp.png" width="150px" alt="" />
+                                    </a>
+                                </div >
+                                <div className="col-xs-12 col-md-6 ">
+                                    <h5><span role="img" aria-label="emojis">👇👇</span></h5>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#telegram">
+                                        <img src="/assets/img/telegram.png" width="150px" alt="" />
+                                    </a>
+                                </div >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Groups;
