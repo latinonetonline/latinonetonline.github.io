c2e1353e92a50d411210208d684d6705df74221d
lautaro <lautaroecarro@gmail.com>
2020-07-30 19:59:12 -0300

    add Discord y Slack

--- src/components/sections/Groups.jsx
+++ src/components/sections/Groups.jsx
@@ -7,20 +7,33 @@ const Groups = () => {
                 <div className="row justify-content-md-center">
                     <div className="col-md-10 col-lg-7">
                         <div className="subscribe-inner wow fadeInDown" data-wow-delay="0.3s">
-                            <h2 className="subscribe-title">Unite a nuestros grupos de WhatsApp y Telegram</h2>
+                            <h2 className="subscribe-title">Unite a nuestros grupos de WhatsApp, Telegram, Slack y Discord</h2>
                             <div className="row">
                                 <div className="col-xs-12 col-md-6 ">
                                     <h5><span role="img" aria-label="emojis">👇👇</span></h5>
-                                    <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#whatsapp">
-                                        <img src="/assets/img/wpp.png" width="150px" alt="" />
+                                    <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#discord">
+                                        <img src="/assets/img/discord.png" width="150px" alt="" />
                                     </a>
-                                </div >
+                                </div>
                                 <div className="col-xs-12 col-md-6 ">
                                     <h5><span role="img" aria-label="emojis">👇👇</span></h5>
+
                                     <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#telegram">
                                         <img src="/assets/img/telegram.png" width="150px" alt="" />
                                     </a>
-                                </div >
+                                </div>
+                                <div className="col-xs-12 col-md-6 ">
+                                    <h5><span role="img" aria-label="emojis"></span></h5>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#slack">
+                                        <img src="/assets/img/slack.png" width="150px" alt="" />
+                                    </a>
+                                </div>
+                                <div className="col-xs-12 col-md-6 ">
+                                    <h5><span role="img" aria-label="emojis"></span></h5>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://latinonet.online/links#whatsapp">
+                                        <img src="/assets/img/wpp.png" width="150px" alt="" />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
