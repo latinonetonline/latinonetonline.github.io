9e67189001fef3112ec482cace93de68a06c793a
Laucha Carro <lautaroecarro@gmail.com>
2021-04-27 21:48:14 -0300

    quite los style inline

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -35,7 +35,7 @@ const BackToTop = () => {
     }
 
     return (
-        <a href="#header-wrap" className="back-to-top" style={{ opacity: opacity, display: display }}>
+        <a href="#header-wrap" className="back-to-top">
             <i className="lni-chevron-up"></i>
         </a>
     )
